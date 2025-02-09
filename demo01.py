e0bc89d186627338fdd742d59059c79b808b9e16
ehrnjica <ehrnjica@outlook.com>
2025-02-09 12:15:52 +0100

    my first program

---
+++ demo01.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(f"Hello, {name}!")
+
+#names = ["Alice", "Bob", "Charlie"]
+
+#
+#for name in names:
+#    greet(name)
+
+input_name = input("Napisi ime koje pozdravljas? ")
+greet(input_name)
